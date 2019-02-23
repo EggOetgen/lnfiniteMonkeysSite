fb3aa294e246c470152aec42c66a473022f1d843
Edmund Oetgen <eoetg002@gold.ac.uk>
2019-02-23 19:29:50 +0000

    attemtping modal

--- javascript/infiniteMonkeys.js
+++ javascript/infiniteMonkeys.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
 
+
+  document.getElementById("bio").innerHTML = "Hello, World!";
+  // Get the modal
+  var modal = document.getElementById('myModal');
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+
+  var modalImg = document.getElementById("img01");
+  var img = document.getElementById("mainImg");
+  var captionText = document.getElementById("caption");
+
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
   var MyBlah = function($blah) {
 
      $("#bio").html("This is some example text about project number one! <br> <br> Nam adipiscing ullamcorper quam. Pellentesque erat. Sed hendrerit. Duis id nisl. Cras arcu mauris, mollis vel, convallis non, elementum a, tortor. Donec ac est eget elit consequat sollicitudin. In id odio quis tortor volutpat mollis. Nulla iaculis lobortis est. ");
@@ -11,7 +35,7 @@ $(document).ready(function() {
   };
 
   var setProject1 = function($blah) {
-
+showOneImg();
     $("#exit").css("color", "#08090A");
 
     $("#title").html($blah);
@@ -23,7 +47,7 @@ $(document).ready(function() {
   };
 
   var setProject2 = function($blah) {
-
+showOneImg();
     $("#exit").css("color", "#08090A");
 
     $("#title").html($blah);
@@ -35,7 +59,7 @@ $(document).ready(function() {
   };
 
   var setProject3 = function($blah) {
-
+showOneImg();
     $("#exit").css("color", "#08090A");
 
     $("#title").html($blah);
@@ -47,7 +71,8 @@ $(document).ready(function() {
   };
 
   var setProject4 = function($blah) {
-    $("img2").css("display", "block");
+    showImg();
+    // $("img2").css("display", "block");
     $("#exit").css("color", "#08090A");
 
     $("#title").html($blah);
@@ -72,13 +97,30 @@ $(document).ready(function() {
   $("#mainImg").html('<img src="images/shell2.jpg" alt="Project Image" class= "mainBoxImg" >');
   };
 
+var hideImg = function() {
+  $("#mainImg").hide();
+  $("#img2").hide();
+
+}
+
+var showImg = function() {
+  $("#mainImg").show();
+  $("#img2").show();
+
+}
+
+var showOneImg = function() {
+  $("#mainImg").show();
+  $("#img2").hide();
+
+}
   var showHome = function() {
     // var newTitle =
     $("img2").css("display", "none");
     $("#title").html("Infinite Monkeys");
     $("#bio").html(" Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse aliquet arcu a ipsum. Nunc arcu est, pulvinar vitae, consectetuer ac, pellentesque interdum, augue. Fusce iaculis nulla et sapien. Aliquam erat volutpat. Etiam iaculis turpis nec libero. Nam adipiscing ullamcorper quam. ");
     // $("#exit").css("color", "#F4F7F5");
-    $("#mainImg").html('<img src="images/us.png" alt="HTML5 Icon" class= "mainBoxImg" >');
+    hideImg();
      $("#exit").html("-");
        // $("#img2").hide();
   };
@@ -91,4 +133,3 @@ $(document).ready(function() {
       $('#project5').click(function(){ setTitle("Project Five"); return false; });
       $('#project6').click(function(){ setTitle("Project Six"); return false; });
 });
-//document.getElementById("bio").innerHTML = "Hello, World!";
